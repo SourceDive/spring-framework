SourceDive/spring-framework

commit afa8b92476dc2558cfefdfbabfe65f6910317f10

Optimize HTTP headers management
Several benchmarks underlined a few hotspots for CPU and GC pressure in
the Spring Framework codebase:

1. `org.springframework.util.MimeType.<init>(String, String, Map)`
2. `org.springframework.util.LinkedCaseInsensitiveMap.convertKey(String)`

Both are linked with HTTP request headers parsing and response headers
writin during the exchange processing phase.

1) is linked to repeated calls to `HttpHeaders.getContentType`
within a single request handling. The media type parsing operation
is expensive and the result doesn't change between calls, since
the request headers are immutable at that point.

This commit improves this by caching the parsed `MediaType` for the
`"Content-Type"` request header in the `ReadOnlyHttpHeaders` class.
This change is available for both Spring MVC and Spring WebFlux.

2) is linked to insertions/lookups in the `LinkedCaseInsensitiveMap`,
which is the data structure behind `HttpHeaders`.
Those operations are creating a lot of garbage (including a lot of
`String` created by `toLowerCase`). We could choose a more efficient
data structure for storing HTTP headers data.

As a first step, this commit is focusing on Spring WebFlux and
introduces `MultiValueMap` implementations mapped by native HTTP headers
for the following servers: Tomcat, Jetty, Netty and Undertow.
Such implementations avoid unnecessary copying of the headers
and leverages as much as possible optimized operations provided by the
native implementations.

This change has a few consequences:

* `HttpHeaders` can now wrap a `MultiValueMap` directly
* The default constructor of `HttpHeaders` is still backed by a
`LinkedCaseInsensitiveMap`
* The HTTP request headers for the websocket HTTP handshake now need to
be cloned, because native headers are likely to be pooled/recycled by
the server implementation, hence gone when the initial HTTP exchange is
done

Issue: SPR-17250

spring-web/src/main/java/org/springframework/http/ReadOnlyHttpHeaders.java
@@ -1,0 +1,135 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.http;
+
+import java.util.AbstractMap;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.lang.Nullable;
+import org.springframework.util.MultiValueMap;
+
+/**
+ * {@code HttpHeaders} object that can only be read, not written to.
+ *
+ * @author Brian Clozel
+ * @since 5.1
+ */
+class ReadOnlyHttpHeaders extends HttpHeaders {
+
+	private static final long serialVersionUID = -8578554704772377436L;
+
+	@Nullable
+	private MediaType cachedContentType;
+
+	ReadOnlyHttpHeaders(HttpHeaders headers) {
+		super(headers.headers);
+	}
+
+	@Override
+	public MediaType getContentType() {
+		if (this.cachedContentType != null) {
+			return this.cachedContentType;
+		}
+		else {
+			MediaType contentType = super.getContentType();
+			this.cachedContentType = contentType;
+			return contentType;
+		}
+	}
+
+	@Override
+	public List<String> get(Object key) {
+		List<String> values = this.headers.get(key);
+		if (values != null) {
+			return Collections.unmodifiableList(values);
+		}
+		return values;
+	}
+
+	@Override
+	public void add(String headerName, @Nullable String headerValue) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void addAll(String key, List<? extends String> values) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void addAll(MultiValueMap<String, String> values) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void set(String headerName, @Nullable String headerValue) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void setAll(Map<String, String> values) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Map<String, String> toSingleValueMap() {
+		return Collections.unmodifiableMap(this.headers.toSingleValueMap());
+	}
+
+	@Override
+	public Set<String> keySet() {
+		return Collections.unmodifiableSet(this.headers.keySet());
+	}
+
+	@Override
+	public List<String> put(String key, List<String> value) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public List<String> remove(Object key) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void putAll(Map<? extends String, ? extends List<String>> map) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void clear() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Collection<List<String>> values() {
+		return Collections.unmodifiableCollection(this.headers.values());
+	}
+
+	@Override
+	public Set<Entry<String, List<String>>> entrySet() {
+		return Collections.unmodifiableSet(this.headers.entrySet().stream()
+				.map(AbstractMap.SimpleImmutableEntry::new)
+				.collect(Collectors.toSet()));
+	}
+
+}

spring-web/src/main/java/org/springframework/http/HttpHeaders.java
@@ -35,8 +35,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.EnumSet;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -47,7 +45,9 @@ import java.util.stream.Collectors;
 
 import org.springframework.lang.Nullable;
 import org.springframework.util.Assert;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.LinkedCaseInsensitiveMap;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.util.StringUtils;
 
@@ -78,7 +78,8 @@ public class HttpHeaders implements MultiValueMap<String, String>, Serializable 
 	/**
 	 * The empty {@code HttpHeaders} instance (immutable).
 	 */
-	public static final HttpHeaders EMPTY = new HttpHeaders(new LinkedHashMap<>(), true);
+	public static final HttpHeaders EMPTY =
+			new ReadOnlyHttpHeaders(new HttpHeaders(new LinkedMultiValueMap<>(0)));
 	/**
 	 * The HTTP {@code Accept} header field name.
 	 * @see <a href="http://tools.ietf.org/html/rfc7231#section-5.3.2">Section 5.3.2 of RFC 7231</a>
@@ -397,35 +398,27 @@ public class HttpHeaders implements MultiValueMap<String, String>, Serializable 
 	private static final DateTimeFormatter[] DATE_FORMATTERS = new DateTimeFormatter[] {
 			DateTimeFormatter.RFC_1123_DATE_TIME,
 			DateTimeFormatter.ofPattern("EEEE, dd-MMM-yy HH:mm:ss zz", Locale.US),
-			DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss yyyy",Locale.US).withZone(GMT)
+			DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss yyyy", Locale.US).withZone(GMT)
 	};
 
 
-	private final Map<String, List<String>> headers;
-
-	private final boolean readOnly;
+	final MultiValueMap<String, String> headers;
 
 
 	/**
-	 * Constructs a new, empty instance of the {@code HttpHeaders} object.
+	 * Construct a new, empty instance of the {@code HttpHeaders} object.
 	 */
 	public HttpHeaders() {
-		this(new LinkedCaseInsensitiveMap<>(8, Locale.ENGLISH), false);
+		this(CollectionUtils.toMultiValueMap(
+				new LinkedCaseInsensitiveMap<>(8, Locale.ENGLISH)));
 	}
 
 	/**
-	 * Private constructor that can create read-only {@code HttpHeader} instances.
+	 * Construct a new {@code HttpHeaders} instance backed by an existing map.
 	 */
-	private HttpHeaders(Map<String, List<String>> headers, boolean readOnly) {
-		if (readOnly) {
-			Map<String, List<String>> map = new LinkedCaseInsensitiveMap<>(headers.size(), Locale.ENGLISH);
-			headers.forEach((key, valueList) -> map.put(key, Collections.unmodifiableList(valueList)));
-			this.headers = Collections.unmodifiableMap(map);
-		}
-		else {
-			this.headers = headers;
-		}
-		this.readOnly = readOnly;
+	public HttpHeaders(MultiValueMap<String, String> headers) {
+		Assert.notNull(headers, "headers must not be null");
+		this.headers = headers;
 	}
 
 
@@ -1474,8 +1467,7 @@ public class HttpHeaders implements MultiValueMap<String, String>, Serializable 
 	@Override
 	@Nullable
 	public String getFirst(String headerName) {
-		List<String> headerValues = this.headers.get(headerName);
-		return (headerValues != null ? headerValues.get(0) : null);
+		return this.headers.getFirst(headerName);
 	}
 
 	/**
@@ -1488,19 +1480,17 @@ public class HttpHeaders implements MultiValueMap<String, String>, Serializable 
 	 */
 	@Override
 	public void add(String headerName, @Nullable String headerValue) {
-		List<String> headerValues = this.headers.computeIfAbsent(headerName, k -> new LinkedList<>());
-		headerValues.add(headerValue);
+		this.headers.add(headerName, headerValue);
 	}
 
 	@Override
 	public void addAll(String key, List<? extends String> values) {
-		List<String> currentValues = this.headers.computeIfAbsent(key, k -> new LinkedList<>());
-		currentValues.addAll(values);
+		this.headers.addAll(key, values);
 	}
 
 	@Override
 	public void addAll(MultiValueMap<String, String> values) {
-		values.forEach(this::addAll);
+		this.headers.addAll(values);
 	}
 
 	/**
@@ -1513,21 +1503,17 @@ public class HttpHeaders implements MultiValueMap<String, String>, Serializable 
 	 */
 	@Override
 	public void set(String headerName, @Nullable String headerValue) {
-		List<String> headerValues = new LinkedList<>();
-		headerValues.add(headerValue);
-		this.headers.put(headerName, headerValues);
+		this.headers.set(headerName, headerValue);
 	}
 
 	@Override
 	public void setAll(Map<String, String> values) {
-		values.forEach(this::set);
+		this.headers.setAll(values);
 	}
 
 	@Override
 	public Map<String, String> toSingleValueMap() {
-		LinkedHashMap<String, String> singleValueMap = new LinkedHashMap<>(this.headers.size());
-		this.headers.forEach((key, valueList) -> singleValueMap.put(key, valueList.get(0)));
-		return singleValueMap;
+		return this.headers.toSingleValueMap();
 	}
 
 
@@ -1623,7 +1609,12 @@ public class HttpHeaders implements MultiValueMap<String, String>, Serializable 
 	 */
 	public static HttpHeaders readOnlyHttpHeaders(HttpHeaders headers) {
 		Assert.notNull(headers, "HttpHeaders must not be null");
-		return (headers.readOnly ? headers : new HttpHeaders(headers, true));
+		if (headers instanceof ReadOnlyHttpHeaders) {
+			return headers;
+		}
+		else {
+			return new ReadOnlyHttpHeaders(headers);
+		}
 	}
 
 }

spring-web/src/main/java/org/springframework/http/server/ServletServerHttpResponse.java
@@ -153,6 +153,9 @@ public class ServletServerHttpResponse implements ServerHttpResponse {
 			Assert.isInstanceOf(String.class, key, "Key must be a String-based header name");
 
 			Collection<String> values1 = servletResponse.getHeaders((String) key);
+			if (headersWritten) {
+				return new ArrayList<>(values1);
+			}
 			boolean isEmpty1 = CollectionUtils.isEmpty(values1);
 
 			List<String> values2 = super.get(key);

spring-web/src/main/java/org/springframework/http/server/reactive/AbstractListenerServerHttpResponse.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,6 +24,7 @@ import reactor.core.publisher.Mono;
 
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.http.HttpHeaders;
 
 /**
  * Abstract base class for listener-based server responses, e.g. Servlet 3.1
@@ -39,6 +40,10 @@ public abstract class AbstractListenerServerHttpResponse extends AbstractServerH
 
 	public AbstractListenerServerHttpResponse(DataBufferFactory dataBufferFactory) {
 		super(dataBufferFactory);
+	}
+
+	public AbstractListenerServerHttpResponse(DataBufferFactory dataBufferFactory, HttpHeaders headers) {
+		super(dataBufferFactory, headers);
 	}
 
 

spring-web/src/main/java/org/springframework/http/server/reactive/AbstractServerHttpResponse.java
@@ -75,9 +75,14 @@ public abstract class AbstractServerHttpResponse implements ServerHttpResponse {
 
 
 	public AbstractServerHttpResponse(DataBufferFactory dataBufferFactory) {
+		this(dataBufferFactory, new HttpHeaders());
+	}
+
+	public AbstractServerHttpResponse(DataBufferFactory dataBufferFactory, HttpHeaders headers) {
 		Assert.notNull(dataBufferFactory, "DataBufferFactory must not be null");
+		Assert.notNull(headers, "HttpHeaders must not be null");
 		this.dataBufferFactory = dataBufferFactory;
-		this.headers = new HttpHeaders();
+		this.headers = headers;
 		this.cookies = new LinkedMultiValueMap<>();
 	}
 

spring-web/src/main/java/org/springframework/http/server/reactive/JettyHeadersAdapter.java
@@ -1,0 +1,222 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.http.server.reactive;
+
+import java.util.AbstractSet;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.eclipse.jetty.http.HttpField;
+import org.eclipse.jetty.http.HttpFields;
+
+import org.springframework.lang.Nullable;
+import org.springframework.util.MultiValueMap;
+
+/**
+ * {@code MultiValueMap} implementation for wrapping Jetty HTTP headers.
+ *
+ * @author Brian Clozel
+ * @since 5.1
+ */
+class JettyHeadersAdapter implements MultiValueMap<String, String> {
+
+	private final HttpFields headers;
+
+	JettyHeadersAdapter(HttpFields headers) {
+		this.headers = headers;
+	}
+
+	@Override
+	public String getFirst(String key) {
+		return this.headers.get(key);
+	}
+
+	@Override
+	public void add(String key, @Nullable String value) {
+		this.headers.add(key, value);
+	}
+
+	@Override
+	public void addAll(String key, List<? extends String> values) {
+		values.forEach(value -> add(key, value));
+	}
+
+	@Override
+	public void addAll(MultiValueMap<String, String> values) {
+		values.forEach(this::addAll);
+	}
+
+	@Override
+	public void set(String key, @Nullable String value) {
+		this.headers.put(key, value);
+	}
+
+	@Override
+	public void setAll(Map<String, String> values) {
+		values.forEach(this::set);
+	}
+
+	@Override
+	public Map<String, String> toSingleValueMap() {
+		Map<String, String> singleValueMap = new LinkedHashMap<>(this.headers.size());
+		Iterator<HttpField> iterator = this.headers.iterator();
+		iterator.forEachRemaining(field -> {
+			if (!singleValueMap.containsKey(field.getName())) {
+				singleValueMap.put(field.getName(), field.getValue());
+			}
+		});
+		return singleValueMap;
+	}
+
+	@Override
+	public int size() {
+		return this.headers.getFieldNamesCollection().size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.headers.size() == 0;
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		if (key instanceof String) {
+			return this.headers.containsKey((String) key);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		if (value instanceof String) {
+			return this.headers.stream()
+					.anyMatch(field -> field.contains((String) value));
+		}
+		return false;
+	}
+
+	@Nullable
+	@Override
+	public List<String> get(Object key) {
+		if (key instanceof String) {
+			return this.headers.getValuesList((String) key);
+		}
+		return null;
+	}
+
+	@Nullable
+	@Override
+	public List<String> put(String key, List<String> value) {
+		List<String> oldValues = get(key);
+		this.headers.put(key, value);
+		return oldValues;
+	}
+
+	@Nullable
+	@Override
+	public List<String> remove(Object key) {
+		if (key instanceof String) {
+			List<String> oldValues = get(key);
+			this.headers.remove((String) key);
+			return oldValues;
+		}
+		return null;
+	}
+
+	@Override
+	public void putAll(Map<? extends String, ? extends List<String>> m) {
+		m.forEach(this::put);
+	}
+
+	@Override
+	public void clear() {
+		this.headers.clear();
+	}
+
+	@Override
+	public Set<String> keySet() {
+		return this.headers.getFieldNamesCollection();
+	}
+
+	@Override
+	public Collection<List<String>> values() {
+		return this.headers.getFieldNamesCollection().stream()
+				.map(this.headers::getValuesList).collect(Collectors.toList());
+	}
+
+	@Override
+	public Set<Entry<String, List<String>>> entrySet() {
+		return new AbstractSet<Entry<String, List<String>>>() {
+			@Override
+			public Iterator<Entry<String, List<String>>> iterator() {
+				return new EntryIterator();
+			}
+
+			@Override
+			public int size() {
+				return headers.size();
+			}
+		};
+	}
+
+	private class EntryIterator implements Iterator<Entry<String, List<String>>> {
+
+		private Enumeration<String> names = headers.getFieldNames();
+
+		@Override
+		public boolean hasNext() {
+			return this.names.hasMoreElements();
+		}
+
+		@Override
+		public Entry<String, List<String>> next() {
+			return new HeaderEntry(this.names.nextElement());
+		}
+	}
+
+	private class HeaderEntry implements Entry<String, List<String>> {
+
+		private final String key;
+
+		HeaderEntry(String key) {
+			this.key = key;
+		}
+
+		@Override
+		public String getKey() {
+			return this.key.toString();
+		}
+
+		@Override
+		public List<String> getValue() {
+			return headers.getValuesList(this.key);
+		}
+
+		@Override
+		public List<String> setValue(List<String> value) {
+			List<String> previousValues = headers.getValuesList(this.key);
+			headers.put(this.key, value);
+			return previousValues;
+		}
+	}
+}

spring-web/src/main/java/org/springframework/http/server/reactive/JettyHttpHandlerAdapter.java
@@ -17,15 +17,21 @@
 package org.springframework.http.server.reactive;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.ByteBuffer;
 import javax.servlet.AsyncContext;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.jetty.http.HttpFields;
 import org.eclipse.jetty.server.HttpOutput;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
 
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.http.HttpHeaders;
 
 /**
  * {@link ServletHttpHandlerAdapter} extension that uses Jetty APIs for writing
@@ -43,6 +49,12 @@ public class JettyHttpHandlerAdapter extends ServletHttpHandlerAdapter {
 
 
 	@Override
+	protected ServletServerHttpRequest createRequest(HttpServletRequest request, AsyncContext context)
+			throws IOException, URISyntaxException {
+		return new JettyServerHttpRequest(request, context, getServletPath(), getDataBufferFactory(), getBufferSize());
+	}
+
+	@Override
 	protected ServletServerHttpResponse createResponse(HttpServletResponse response,
 			AsyncContext context, ServletServerHttpRequest request) throws IOException {
 
@@ -50,14 +62,38 @@ public class JettyHttpHandlerAdapter extends ServletHttpHandlerAdapter {
 				response, context, getDataBufferFactory(), getBufferSize(), request);
 	}
 
+	private static final class JettyServerHttpRequest extends ServletServerHttpRequest {
+
+		JettyServerHttpRequest(HttpServletRequest request, AsyncContext asyncContext,
+				String servletPath, DataBufferFactory bufferFactory, int bufferSize)
+				throws IOException, URISyntaxException {
+
+			super(createHeaders(request), request, asyncContext, servletPath, bufferFactory, bufferSize);
+		}
+
+		private static HttpHeaders createHeaders(HttpServletRequest request) {
+			HttpFields fields = ((Request) request).getMetaData().getFields();
+			return new HttpHeaders(new JettyHeadersAdapter(fields));
+		}
+	}
+
 
 	private static final class JettyServerHttpResponse extends ServletServerHttpResponse {
 
-		public JettyServerHttpResponse(HttpServletResponse response, AsyncContext asyncContext,
+		JettyServerHttpResponse(HttpServletResponse response, AsyncContext asyncContext,
 				DataBufferFactory bufferFactory, int bufferSize, ServletServerHttpRequest request)
 				throws IOException {
 
-			super(response, asyncContext, bufferFactory, bufferSize, request);
+			super(createHeaders(response), response, asyncContext, bufferFactory, bufferSize, request);
+		}
+
+		private static HttpHeaders createHeaders(HttpServletResponse response) {
+			HttpFields fields = ((Response) response).getHttpFields();
+			return new HttpHeaders(new JettyHeadersAdapter(fields));
+		}
+
+		@Override
+		protected void applyHeaders() {
 		}
 
 		@Override

commit 4b080bc3bf439e426e079f935a2e04be9defb323

Retain entry set order in read-only HttpHeaders
Prior to this commit, the entry set of read-only HttpHeaders lost the
original headers' ordering.

The changes in commit afa8b92476 introduced a regression in the read-only
HttpHeaders support. Specifically, the implementation of entrySet() in
the internal ReadOnlyHttpHeaders class converted the original entry set
to an immutable, non-ordered set of immutable entries.

This commit fixes this issue by converting the original entry set to an
immutable, ordered set of immutable entries.

Closes gh-23551

spring-web/src/main/java/org/springframework/http/ReadOnlyHttpHeaders.java
@@ -16,9 +16,10 @@
 
 package org.springframework.http;
 
-import java.util.AbstractMap;
+import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -31,6 +32,7 @@ import org.springframework.util.MultiValueMap;
  * {@code HttpHeaders} object that can only be read, not written to.
  *
  * @author Brian Clozel
+ * @author Sam Brannen
  * @since 5.1.1
  */
 class ReadOnlyHttpHeaders extends HttpHeaders {
@@ -141,9 +143,10 @@ class ReadOnlyHttpHeaders extends HttpHeaders {
 
 	@Override
 	public Set<Entry<String, List<String>>> entrySet() {
-		return Collections.unmodifiableSet(this.headers.entrySet().stream()
-				.map(AbstractMap.SimpleImmutableEntry::new)
-				.collect(Collectors.toSet()));
+		return this.headers.entrySet().stream().map(SimpleImmutableEntry::new)
+				.collect(Collectors.collectingAndThen(
+						Collectors.toCollection(LinkedHashSet::new), // Retain original ordering of entries
+						Collections::unmodifiableSet));
 	}
 
 }

spring-web/src/test/java/org/springframework/http/HttpHeadersTests.java
@@ -32,6 +32,7 @@ import java.util.EnumSet;
 import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map.Entry;
 import java.util.TimeZone;
 
 import org.hamcrest.Matchers;
@@ -47,6 +48,7 @@ import static org.junit.Assert.*;
  * @author Sebastien Deleuze
  * @author Brian Clozel
  * @author Juergen Hoeller
+ * @author Sam Brannen
  */
 public class HttpHeadersTests {
 
@@ -555,6 +557,22 @@ public class HttpHeadersTests {
 		headers.setBearerAuth(token);
 		String authorization = headers.getFirst(HttpHeaders.AUTHORIZATION);
 		assertEquals("Bearer foo", authorization);
+	}
+
+	@Test
+	public void readOnlyHttpHeadersRetainEntrySetOrder() {
+		headers.add("aardvark", "enigma");
+		headers.add("beaver", "enigma");
+		headers.add("cat", "enigma");
+		headers.add("dog", "enigma");
+		headers.add("elephant", "enigma");
+
+		String[] expectedKeys = new String[] { "aardvark", "beaver", "cat", "dog", "elephant" };
+
+		assertArrayEquals(expectedKeys, headers.entrySet().stream().map(Entry::getKey).toArray());
+
+		HttpHeaders readOnlyHttpHeaders = HttpHeaders.readOnlyHttpHeaders(headers);
+		assertArrayEquals(expectedKeys, readOnlyHttpHeaders.entrySet().stream().map(Entry::getKey).toArray());
 	}
 
 }