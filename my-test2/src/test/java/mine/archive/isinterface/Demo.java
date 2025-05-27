5bef53edef0caf47357cad7551a1de050f3f2523
ongoing-z <code@ongoingblog.org>
2025-05-27 10:24:50 +0800

    添加测试程序。

---
+++ my-test2/src/test/java/mine/archive/isinterface/Demo.java
@@ -0,0 +1,9 @@
+package mine.archive.isinterface;
+
+/**
+ * @author zero
+ * @description todo
+ * @date 2025-05-27
+ */
+public interface Demo {
+}
---
+++ my-test2/src/test/java/mine/archive/isinterface/IsInstaceDemo.java
@@ -0,0 +1,12 @@
+package mine.archive.isinterface;
+
+/**
+ * @author zero
+ * @description 判断是否是接口类型
+ * @date 2025-05-27
+ */
+public class IsInstaceDemo {
+	public static void main(String[] args) {
+		System.out.println(Demo.class.isInterface());
+	}
+}
