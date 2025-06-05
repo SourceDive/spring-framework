fbcec4ddd96abaa26a0b8990449723c9374fdd55
ongoing-z <code@ongoingblog.org>
2025-06-05 14:34:15 +0800

    添加测试程序。

---
+++ my-test2/src/test/java/mine/archive/marker_interface/TestInterface.java
@@ -0,0 +1,10 @@
+package mine.archive.marker_interface;
+
+/**
+ * @author zero
+ * @description 测试空接口和default关键字
+ * @date 2025-06-05
+ */
+public interface TestInterface {
+	default void newExec() {}
+}
---
+++ my-test2/src/test/java/mine/archive/marker_interface/TestInterfaceImpl.java
@@ -0,0 +1,9 @@
+package mine.archive.marker_interface;
+
+/**
+ * @author zero
+ * @description todo
+ * @date 2025-06-05
+ */
+public class TestInterfaceImpl implements TestInterface {
+}
