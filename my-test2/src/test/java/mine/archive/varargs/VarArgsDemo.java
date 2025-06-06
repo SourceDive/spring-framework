f6fe1aa9a9b40a2b4a9d403f4eae893a85ecdea9
ongoing-z <code@ongoingblog.org>
2025-06-06 13:30:25 +0800

    添加测试程序。

---
+++ my-test2/src/test/java/mine/archive/varargs/VarArgsDemo.java
@@ -0,0 +1,21 @@
+package mine.archive.varargs;
+
+/**
+ * @author zero
+ * @description 测试可变参数
+ * @date 2025-06-06
+ */
+public class VarArgsDemo {
+
+	public static void exec(String... params) { // 这里就相当于 String[] params
+		System.out.println(params.length);
+		for (String str: params) {
+			System.out.println(str);
+		}
+
+	}
+
+	public static void main(String[] args) {
+		exec("01", "02");
+	}
+}
