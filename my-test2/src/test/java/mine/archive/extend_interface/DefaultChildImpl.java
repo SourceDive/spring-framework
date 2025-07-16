b5d14c78ef77591a75c8b7b8c329e3e72ee80aca
ongoing-z <code@ongoingblog.org>
2025-07-16 15:48:41 +0800

    添加测试程序。

---
+++ my-test2/src/test/java/mine/archive/extend_interface/DefaultChildImpl.java
@@ -0,0 +1,18 @@
+package mine.archive.extend_interface;
+
+/**
+ * @author zero
+ * @description 子接口的实现类也要实现父接口的方法
+ * @date 2025-07-16
+ */
+public class DefaultChildImpl implements ChildInterface {
+	@Override
+	public void methodChild() {
+
+	}
+
+	@Override
+	public void methodParent() {
+
+	}
+}
---
+++ my-test2/src/test/java/mine/archive/extend_interface/ParentInterface.java
@@ -0,0 +1,10 @@
+package mine.archive.extend_interface;
+
+/**
+ * @author zero
+ * @description todo
+ * @date 2025-07-16
+ */
+public interface ParentInterface {
+	void methodParent();
+}
