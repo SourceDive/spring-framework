305d3302a682139faf9c69c88c4795e3d73e401f
ongoing-z <code@ongoingblog.org>
2025-07-16 13:09:50 +0800

    优化测试程序。

--- my-test2/src/test/java/mine/projects/transaction/h2/jdbctemplate/service/UserDao.java
+++ my-test2/src/test/java/mine/projects/transaction/h2/jdbctemplate/service/UserDao.java
@@ -1,12 +1,13 @@
 package mine.projects.transaction.h2.jdbctemplate.service;
 
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 // 服务接口
 public interface UserDao {
-	@Transactional
+	@Transactional(propagation = Propagation.REQUIRED)
 	void createUser(String username);
 
 	List<String> getAllUserName();
