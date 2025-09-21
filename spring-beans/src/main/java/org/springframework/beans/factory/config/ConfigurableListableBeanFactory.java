ae465b0fa98fd2c904ef4f946647fffa9583f904
ongoing-z <code@ongoingblog.org>
2025-09-21 17:01:21 +0800

    添加注释。

--- spring-beans/src/main/java/org/springframework/beans/factory/config/ConfigurableListableBeanFactory.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/config/ConfigurableListableBeanFactory.java
@@ -150,8 +150,9 @@ public interface ConfigurableListableBeanFactory
 	 */
 	boolean isConfigurationFrozen();
 
-	// 保证所有非懒加载单例及FactoryBean的初始化。
 	/**
+	 * <p>保证所有非懒加载单例及FactoryBean的初始化。</p>
+	 * <p>需要延迟的则直接跳过，显示getBean()时再去创建。</p>
 	 * Ensure that all non-lazy-init singletons are instantiated, also considering
 	 * {@link org.springframework.beans.factory.FactoryBean FactoryBeans}.
 	 * Typically invoked at the end of factory setup, if desired.
