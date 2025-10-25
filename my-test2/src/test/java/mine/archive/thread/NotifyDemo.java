1fe199a83a51f7a24052e6c959776350eee71ea4
ongoing-z <code@ongoingblog.org>
2025-10-25 17:01:02 +0800

    添加测试程序。

---
+++ my-test2/src/test/java/mine/archive/thread/NotifyDemo.java
@@ -0,0 +1,35 @@
+package mine.archive.thread;
+
+/**
+ * @author zero
+ * @description 测试 wait 和 notify 方法。
+ * @date 2025-10-25
+ */
+public class NotifyDemo {
+	public static void main(String[] args) throws InterruptedException {
+		Object lock = new Object();
+
+		Thread t1 = new Thread(() -> {
+			try {
+				synchronized (lock) {
+					lock.wait();
+				}
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+			System.out.println(Thread.currentThread().getName());
+		}, "线程01");
+		Thread t2 = new Thread(() -> {
+			System.out.println(Thread.currentThread().getName());
+			synchronized (lock) {
+				lock.notify();
+			}
+		}, "线程02");
+
+		t1.start();
+		t2.start();
+
+		System.out.println(Thread.currentThread().getName());
+		Thread.sleep(10000);
+	}
+}
