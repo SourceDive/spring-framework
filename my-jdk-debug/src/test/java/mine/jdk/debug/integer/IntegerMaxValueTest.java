4b0308914a36215f348275e0e9691460161c6e36
ongoing-z <code@ongoingblog.org>
2025-12-15 14:59:28 +0800

    添加测试案例。

---
+++ my-jdk-debug/src/test/java/mine/jdk/debug/integer/IntegerMaxValueTest.java
@@ -0,0 +1,12 @@
+package mine.jdk.debug.integer;
+
+/**
+ * @author zero
+ * @description 查看 0x7FFFFFFF
+ * @date 2025-12-15
+ */
+public class IntegerMaxValueTest {
+	public static void main(String[] args) {
+		System.out.println(0x7FFFFFFF == Integer.MAX_VALUE);
+	}
+}
